302a91445779a89d4995cb859e261c014197ad5e
azadeh.samadi <samadi.azadeh@gmail.com>
2023-11-22 13:23:12 +0100

    first commit

---
+++ exersice.js
@@ -0,0 +1,22 @@
+//exersice1
+let text = "Hello, world!";
+console.log(text.length);
+console.log(text.toUpperCase());
+console.log(text.indexOf('world'));
+console.log(text.slice(0.5));
+
+
+const currentDate = new Date();
+console.log (currentDate);  
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,20 @@
+// Javascript Variables
+let age = 25;
+let temperature= -3.5;
+
+let name = "Azadeh";
+let message = "Hello Javascript";
+console.log(message);
+
+let myvar = 10; // number
+myvar = "10"; // string
+
+let isRaining = true; // or false
+
+let person = { id:1, name : "Alis", isStudent:false };
+let numbers = [1,2,3,4,5];
+
+let nulvar = null;
+let description;
+
+
